f894a5b4ebe77c1ec0db8800e16945a000f90c53
Kipland Melton <kiplandmelton@gmail.com>
2025-03-29 14:24:19 -0500

    now reading header data and printing image size

--- src/main.c
+++ src/main.c
@@ -6,25 +6,29 @@ struct jpeg_decompress_struct decomp;
 struct jpeg_error_mgr jpeg_err;
 FILE* fptr;
 
-void load_jpeg() {
-
-}
-
 int main(int argc, char *argv[]) {
-  // initializing error handler
+  /* initializing error handler */
   decomp.err = jpeg_std_error(&jpeg_err);
   jpeg_create_decompress(&decomp);
 
-  printf("%d\n", argc);
+  /* printf("%d\n", argc); */
 
   fptr = fopen("test-images/MoonKilledCthun.jpg", "rb");
-
   if (fptr == NULL) {
     printf("File was not found\n");
     exit(0);
   } else {
     printf("File loaded successfully\n");
   }
+  
+  /* providing file pointer to decompression struct */
+  jpeg_stdio_src(&decomp, fptr);
+
+  (void) jpeg_read_header(&decomp, TRUE);
+
+  printf("%d\n", decomp.image_height);
+  printf("%d\n", decomp.image_width);
+
 
   return 0;
 }
