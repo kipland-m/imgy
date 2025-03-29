2b5026fc990a9fe416dd8e0953fff3aee4956dcd
Kipland Melton <kiplandmelton@gmail.com>
2025-03-29 13:54:50 -0500

    initialzing decompression struct for jpeg

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <jpeglib.h>
-#include <libpng.h>
 
-
+struct jpeg_decompress_struct decomp;
+struct jpeg_error_mgr jpeg_err;
 FILE* fptr;
 
+void load_jpeg() {
+
+}
+
 int main(int argc, char *argv[]) {
+  // initializing error handler
+  decomp.err = jpeg_std_error(&jpeg_err);
+  jpeg_create_decompress(&decomp);
+
   printf("%d\n", argc);
 
   fptr = fopen("test-images/MoonKilledCthun.jpg", "rb");
