3b80162084fc6278549940877dd7775f9ad7697b
Kipland Melton <kiplandmelton@gmail.com>
2025-04-05 19:12:54 -0500

    refactoring, about to start writing to buffer

--- src/main.c
+++ src/main.c
@@ -2,36 +2,41 @@
 #include <stdlib.h>
 #include <jpeglib.h>
 
-struct jpeg_decompress_struct decomp;
-struct jpeg_error_mgr jpeg_err;
-FILE* fptr;
-
-/*
- *  In order to read a jpeg file, we will need to provide a file and tell it to decompress the file
- *  via the decompression struct
+/* open_file is just separating our file opening logic
  */
-
-void read_jpeg_file(struct jpeg_decompress_struct decomp, char *filepath) {
-  /* initializing error handler */
-  decomp.err = jpeg_std_error(&jpeg_err);
-  jpeg_create_decompress(&decomp);
-
+FILE* open_file(char *filepath) { 
   if (filepath == NULL) {
     printf("Missing filepath?\n");
   }
 
-  fptr = fopen(filepath, "rb");
+  FILE* infptr = fopen(filepath, "rb");
 
-  if (fptr == NULL) {
+  if (infptr == NULL) {
     printf("File was not found\n");
-    exit(0);
+    exit(1);
   } else {
     printf("File loaded successfully\n");
+    return infptr;
   }
-  
-  /* providing file pointer to decompression struct */
-  jpeg_stdio_src(&decomp, fptr);
+}
 
+/* do_read_jpeg will handle reading any data related to our image.
+ * (header data, pixel data)
+ */
+int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *outfilepath) {
+  struct jpeg_error_mgr jpeg_err;
+
+  /* we are initialzing jpeg_err with jpeg_std_error(),
+   * and sets decomp's "err" to our jpeg_err 
+   */
+  decomp.err = jpeg_std_error(&jpeg_err);
+  jpeg_create_decompress(&decomp);
+
+  /* providing file pointer to decompression struct */
+  FILE* infptr = open_file(infilepath); 
+  jpeg_stdio_src(&decomp, infptr);
+
+  /* we are giving our decompression struct some header data */
   (void) jpeg_read_header(&decomp, TRUE);
 
   printf("%d\n", decomp.image_height);
@@ -40,12 +45,17 @@ void read_jpeg_file(struct jpeg_decompress_struct decomp, char *filepath) {
 
   (void) jpeg_start_decompress(&decomp);
 
-  
+  return 0;
+}
 
+int read_jpeg(char *infile, char *outfile) {
+  struct jpeg_decompress_struct decomp;
+
+  return do_read_jpeg(decomp, infile, outfile); 
 }
 
 int main(int argc, char *argv[]) {
-  read_jpeg_file(decomp, argv[1]);
+  read_jpeg(argv[1], argv[2]);
 
   return 0;
 }
