19fc490b20bbba3240f4966eb11e61a2e9ecde4d
Kipland Melton <kiplandmelton@gmail.com>
2025-04-13 23:34:44 -0500

    removed dumbass redundant comments (they will probably be back)

--- src/main.c
+++ src/main.c
@@ -34,20 +34,22 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
   /* providing infile pointer to decompression struct */
   jpeg_stdio_src(&decomp, open_file(infilepath));
 
-  /* we are giving our decompression struct some header data */
   (void) jpeg_read_header(&decomp, TRUE);
-
-  /* DEBUG */
   printf("%d\n", decomp.image_height);
   printf("%d\n", decomp.image_width);
   printf("%d\n", decomp.num_components); 
 
   (void) jpeg_start_decompress(&decomp);
 
+  /* row_stride = the length of a row of pixels in our image.
+   * a pixel will be represented by 3 sets of digits (R,G,B)
+   *
+   * ex. MoonKilledCthun.jpg
+   * total amount of pixels in row_stride: 3360 * 3 = 10080
+   */
   row_stride = decomp.output_width * decomp.output_components;
 
-  /* allocating big ass buffer to store whole image
-   * in the case of MoonKilledCthun.jpg, it is ~21 million bytes large
+  /* allocating big ass buffer to store whole image RGB data
    */
   full_buffer = malloc(decomp.output_width * decomp.output_height * decomp.output_components);
   printf("full_buffer size: %d\n", decomp.output_width * decomp.output_height * decomp.output_components);
@@ -55,27 +57,19 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
   for (i = 0; i < decomp.output_height; i++) {
     /* scary pointer arithmetic, jumping forward <row length> amount in bytes */
     row_pointers[i] = full_buffer + (i * row_stride);
+
+    /*
     printf("row %d: %p\n", i, (void *)row_pointers[i]);
+    */
   }
  
   /* write into buffer here */
   while (decomp.output_scanline < decomp.output_height) {
-
     int rows_read = jpeg_read_scanlines(&decomp, &row_pointers[decomp.output_scanline], 1);
 
     /*
-    printf("output scanline: %d\n", decomp.output_scanline);
-    printf("full buffer[%d]: %d\n", 10080, full_buffer[10080]);
-    printf("full buffer[%d]: %d\n", 10081, full_buffer[10081]);
-    printf("full buffer[%d]: %d\n", 10082, full_buffer[10082]);
-    printf("full buffer[%d]: %d\n", 10083, full_buffer[10083]);
-    printf("full buffer[%d]: %d\n", 10084, full_buffer[10084]);
-    printf("full buffer[%d]: %d\n", 10085, full_buffer[10085]);
-    printf("row %d: %d\n", decomp.output_scanline, full_buffer[decomp.output_scanline * row_stride]);
-    printf("rows read: %d\n", rows_read);
     printf("%d %d %d\n", full_buffer[0], full_buffer[10080], full_buffer[20160]); 
-  */
-
+    */
   }
   printf("\n");
 
@@ -83,7 +77,6 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
 }
 
 int read_jpeg(char *infile, char *outfile) {
-  /* Creating and initializing decompression struct */
   struct jpeg_decompress_struct decomp;
   jpeg_create_decompress(&decomp);
 
