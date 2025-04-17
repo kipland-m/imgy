9ba1ecec995c9d833da2cafe744b9a7e38a803c4
Kipland Melton <kiplandmelton@gmail.com>
2025-04-16 19:11:20 -0500

    update

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
 
   int i;
   int row_stride;
-  unsigned char * full_buffer = NULL;
+  unsigned char *full_buffer = NULL;
   JSAMPARRAY row_pointers = malloc(decomp.output_height * sizeof(unsigned char *)); /* will store pointers of all rows of full_buffer */
 
   /* providing infile pointer to decompression struct */
@@ -44,7 +44,7 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
   /* row_stride = the length of a row of pixels in our image.
    * a pixel will be represented by 3 sets of digits (R,G,B)
    *
-   * ex. MoonKilledCthun.jpg
+   * ex. MoonKilledCthun.jpg (3360x2100)
    * total amount of pixels in row_stride: 3360 * 3 = 10080
    */
   row_stride = decomp.output_width * decomp.output_components;
@@ -56,6 +56,10 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
 
   for (i = 0; i < decomp.output_height; i++) {
     /* scary pointer arithmetic, jumping forward <row length> amount in bytes */
+    /* so, I had to revisit this, because my lack of understanding was bothering me.
+     * we have to realize that full_buffer is a POINTER to an ARRAY of bytes. 
+     * full_buffer[0] will refer to our FIRST byte in our allocated buffer to store
+     * a provided image.*/
     row_pointers[i] = full_buffer + (i * row_stride);
     /*
     printf("row %d: %p\n", i, (void *)row_pointers[i]);
@@ -66,12 +70,24 @@ int do_read_jpeg(struct jpeg_decompress_struct decomp, char *infilepath, char *o
   while (decomp.output_scanline < decomp.output_height) {
     int rows_read = jpeg_read_scanlines(&decomp, &row_pointers[decomp.output_scanline], 1);
   }
-
-  printf("pixel 0: R%d G%d B%d\n", full_buffer[0], full_buffer[1], full_buffer[2]); 
+  printf("pixel 0: R%d G%d B%d\n", full_buffer[0], full_buffer[1], full_buffer[2]); /* first pixel first row */
+  printf("pixel 3360: R%d G%d B%d\n", full_buffer[10078], full_buffer[10079], full_buffer[10080]); /* last pixel first row */
 
   return 0;
 }
 
+void resize_jpeg(unsigned char *input_buffer) {
+  /* testing implementation of nearest neighbor with hardcoded
+   * output size
+   */
+  int OUTPUT_WIDTH = 800;
+  int OUTPUT_HEIGHT = 600;
+
+
+
+}
+
+/* need to add desired output size, etc */
 int read_jpeg(char *infile, char *outfile) {
   struct jpeg_decompress_struct decomp;
   jpeg_create_decompress(&decomp);
