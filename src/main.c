acfe6c33258a74111edbca47bf016cea112466f7
Kipland Melton <kiplandmelton@gmail.com>
2025-06-08 23:38:03 -0500

    trying to fix bug with lower part of image colors being messed up..

--- src/main.c
+++ src/main.c
@@ -43,8 +43,8 @@ void resize_jpeg(struct jpeg_decompress_struct *decomp,
                   unsigned char *input_buffer, char *outfilepath,
                   int arg_width, int arg_height) {
 
-  int OUTPUT_WIDTH = arg_width;  
-  int OUTPUT_HEIGHT = arg_height;
+  int OUTPUT_WIDTH = 800;  
+  int OUTPUT_HEIGHT = 600;
 
   int i;
   int j;
@@ -60,10 +60,12 @@ void resize_jpeg(struct jpeg_decompress_struct *decomp,
 
   resize_buffer = malloc(OUTPUT_WIDTH * OUTPUT_HEIGHT * decomp->output_components);
 
+  /*
   for (i = 0; i < OUTPUT_WIDTH; i++) {
     source_x_array[i] = round((float)i / OUTPUT_WIDTH * decomp->output_width);
     printf("%f\n", source_x_array[i]);
   }
+  */
 
   for (i = 0; i < OUTPUT_HEIGHT; i++) {
     row_pointers[i] = resize_buffer + (i * row_stride);
